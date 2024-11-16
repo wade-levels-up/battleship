fa4bd222e71f6d0e46d87876bbf586619e86ecdb
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-11-16 19:19:25 +1100

    feat(Ship/test): Add more tests for Ship instances
    Tests confirm ships take and register hits and
    that when the amount of hits is the same as the
    size of the ship that the ship registers as sunk.

--- src/ship.js
+++ src/ship.js
@@ -2,9 +2,15 @@ export default class Ship {
   constructor(size) {
     this.size = size;
     this.hits = 0;
+    this.sunk = false;
   }
 
   takeHit() {
     this.hits += 1;
+    this.isSunk();
+  }
+
+  isSunk() {
+    if (this.hits >= this.size) this.sunk = true;
   }
 }
--- src/ship.test.js
+++ src/ship.test.js
@@ -10,7 +10,20 @@ test('Test that ship registers hits', () => {
   expect(ship1.hits).toBe(3);
 });
 
-test('Test that ship is size specified as parameter', () => {
+test('Test that ship size can be set', () => {
   const ship2 = new Ship(5);
   expect(ship2.size).toBe(5);
+});
+
+test('Test that ships can be sunk', () => {
+  const ship0 = new Ship(3);
+  ship0.takeHit();
+  ship0.takeHit();
+  ship0.takeHit();
+  expect(ship0.sunk).toBe(true);
+});
+
+test(`Test that ships aren't sunk if enough hits aren't taken`, () => {
+  const ship1 = new Ship(3);
+  expect(ship1.sunk).toBe(false);
 });
