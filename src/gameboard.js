70bdab15e990b1627f2ee1e4605495b87b7cfb07
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-11-17 15:41:47 +1100

    feat(gameboard): Setup gameboard with border nodes
    When the gameboard is initialized it is now done
    so with the nodes on the borders having their
    edges set appropriately to null where an edge
    doesn't exist. For example, the top right corner
    node 99 doesn't have an edge to the north or to
    the east, therefore it's edges for north and east
    will be set to null.

--- src/gameboard.js
+++ src/gameboard.js
@@ -6,9 +6,66 @@ export default class Gameboard {
   // eslint-disable-next-line class-methods-use-this
   initializeNodes() {
     const nodeArr = [];
-    for (let x = 0; x < 10; x += 1) {
-      for (let y = 0; y < 10; y += 1) {
-        nodeArr.push({ vertex: [0 + y, 0 + x] });
+    for (let y = 0; y < 10; y += 1) {
+      for (let x = 0; x < 10; x += 1) {
+        // Handle corner nodes first
+        if (x === 0 && y === 0) {
+          nodeArr.push({
+            data: 'water',
+            vertex: [0 + x, 0 + y],
+            edges: { north: [0, 0], east: [0, 0], south: [null], west: [null] },
+          });
+        } else if (x === 0 && y === 9) {
+          nodeArr.push({
+            data: 'water',
+            vertex: [0 + x, 0 + y],
+            edges: { north: [null], east: [0, 0], south: [0, 0], west: [null] },
+          });
+        } else if (x === 9 && y === 0) {
+          nodeArr.push({
+            data: 'water',
+            vertex: [0 + x, 0 + y],
+            edges: { north: [0, 0], east: [null], south: [null], west: [0, 0] },
+          });
+        } else if (x === 9 && y === 9) {
+          nodeArr.push({
+            data: 'water',
+            vertex: [0 + x, 0 + y],
+            edges: { north: [null], east: [null], south: [0, 0], west: [0, 0] },
+          });
+        } else if (x === 0) {
+          // Then handle border nodes
+          nodeArr.push({
+            data: 'water',
+            vertex: [0 + x, 0 + y],
+            edges: { north: [0, 0], east: [0, 0], south: [0, 0], west: [null] },
+          });
+        } else if (y === 0) {
+          nodeArr.push({
+            data: 'water',
+            vertex: [0 + x, 0 + y],
+            edges: { north: [0, 0], east: [0, 0], south: [null], west: [0, 0] },
+          });
+        } else if (x === 9) {
+          nodeArr.push({
+            data: 'water',
+            vertex: [0 + x, 0 + y],
+            edges: { north: [0, 0], east: [null], south: [0, 0], west: [0, 0] },
+          });
+        } else if (y === 9) {
+          nodeArr.push({
+            data: 'water',
+            vertex: [0 + x, 0 + y],
+            edges: { north: [null], east: [0, 0], south: [0, 0], west: [0, 0] },
+          });
+        } else {
+          // Then handle nodes not on the border
+          nodeArr.push({
+            data: 'water',
+            vertex: [0 + x, 0 + y],
+            edges: { north: [0, 0], east: [0, 0], south: [0, 0], west: [0, 0] },
+          });
+        }
       }
     }
     return nodeArr;
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -18,7 +18,7 @@ test(`Each node on the gameboard has coordinates for it's vertex position`, () =
   });
 });
 
-test(`Each Nodes's vertex position is a combination of numbers between 0-9`, () => {
+test(`Each nodes's vertex position is a combination of two numbers between 0-9`, () => {
   const gameboard1 = new Gameboard();
   const [nodes] = [gameboard1.nodes];
   const boardRange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -41,4 +41,58 @@ test(`Each node has unique vertex coordinates starting at [0,0] through to [9,9]
   // Test last node
   expect(gameboard1.nodes[99].vertex[0]).toBe(9);
   expect(gameboard1.nodes[99].vertex[1]).toBe(9);
+});
+
+test('Each node has a data property with a value', () => {
+  const gameboard1 = new Gameboard();
+  const [nodes] = [gameboard1.nodes];
+  nodes.forEach((node) => {
+    expect(node.data).toBeTruthy();
+  });
+});
+
+test('Nodes edges contain values', () => {
+  const gameboard1 = new Gameboard();
+  const [nodes] = [gameboard1.nodes];
+  nodes.forEach((node) => {
+    expect(node.edges.north).toBeTruthy();
+    expect(node.edges.east).toBeTruthy();
+    expect(node.edges.south).toBeTruthy();
+    expect(node.edges.west).toBeTruthy();
+  });
+});
+
+test('x and y coordinates for nodes correspond with x and y axis', () => {
+  const gameboard1 = new Gameboard();
+  const [nodes] = [gameboard1.nodes];
+  expect(nodes[12].vertex[0]).toBe(2);
+  expect(nodes[12].vertex[1]).toBe(1);
+  expect(nodes[25].vertex[0]).toBe(5);
+  expect(nodes[25].vertex[1]).toBe(2);
+});
+
+test(`Border nodes have null in one or more edges, otherwise it's not a border node and no edge should be null`, () => {
+  const gameboard1 = new Gameboard();
+  const [nodes] = [gameboard1.nodes];
+  nodes.forEach((node) => {
+    if (node.vertex[0] === 0) {
+      // If node is on the left border, west edge should be null
+      expect(node.edges.west).toEqual([null]);
+    } else if (node.vertex[0] === 9) {
+      // If node is on the right border, east edge should be null
+      expect(node.edges.east).toEqual([null]);
+    } else if (node.vertex[1] === 0) {
+      // If node is on the bottom border, south edge should be null
+      expect(node.edges.south).toEqual([null]);
+    } else if (node.vertex[1] === 9) {
+      // If node is on the top border, north edge should be null
+      expect(node.edges.north).toEqual([null]);
+    } else {
+      // node isn't on a border, therefore no edge should be null
+      expect(node.edges.north).not.toEqual([null]);
+      expect(node.edges.east).not.toEqual([null]);
+      expect(node.edges.south).not.toEqual([null]);
+      expect(node.edges.west).not.toEqual([null]);
+    }
+  });
 });
