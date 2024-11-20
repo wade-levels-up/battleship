e736f97a0efd7cc4fd01ffdee0f46b92479bf1ee
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-11-20 18:25:37 +1100

    feat(player): Add gameboard to Player class

--- src/player.js
+++ src/player.js
@@ -1,5 +1,8 @@
+import Gameboard from './gameboard';
+
 export default class Player {
   constructor(type) {
     this.type = type;
+    this.gameboard = new Gameboard();
   }
 }
--- src/player.test.js
+++ src/player.test.js
@@ -2,5 +2,13 @@ import Player from './player';
 
 test('Player can either be real or a computer', () => {
   const player1 = new Player('real');
+  const player2 = new Player('computer');
+
   expect(player1.type).toBe('real');
+  expect(player2.type).toBe('computer');
+});
+
+test('Players contain their own gameboard', () => {
+  const player1 = new Player();
+  expect(player1.gameboard).toBeTruthy();
 });
