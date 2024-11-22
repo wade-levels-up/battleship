d5b8c810f3897b034fe95ad4da421f02d1314a52
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-11-22 19:13:05 +1100

    feat(UI): Enhance UI, Make switch view button work

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import { renderGridPlayer, renderGridComputer } from './renderer';
 let player1;
 let player2;
 
-let tries = 100;
+let tries = 1;
+let activeView = 'Enemy Waters';
 
 const commentary = document.querySelector('.commentary');
 const screen = document.querySelector('.screenMode');
 const playerGrid = document.querySelector('.player-grid');
 const computerGrid = document.querySelector('.computer-grid');
+const switchView = document.querySelector('.changeView');
 playerGrid.setAttribute('data-player', 'player');
 computerGrid.setAttribute('data-player', 'computer');
 
@@ -26,15 +28,28 @@ function hideDOMGameboard(gameboardContainer) {
   gameboardElement.style.zIndex = -1;
 }
 
-function setupNewGame() {
-  commentary.innerText = 'Choose a position to attack';
-  screen.innerText = 'View: Enemy Waters';
+switchView.addEventListener('click', () => {
+  if (activeView === 'Enemy Waters') {
+    activeView = 'Your Fleet';
+    revealDOMGameboard(playerGrid);
+    hideDOMGameboard(computerGrid);
+    screen.innerText = 'View: Your Fleet';
+    commentary.innerText = `Switch view to choose attack`;
+  } else {
+    activeView = 'Enemy Waters';
+    revealDOMGameboard(computerGrid);
+    hideDOMGameboard(playerGrid);
+    screen.innerText = 'View: Enemy Waters';
+    commentary.innerText = 'Choose a position to attack';
+  }
+});
 
+function setupNewGame() {
   player1 = new Player('real', true);
   player2 = new Player('computer', false);
 
-  playerGrid.innerHTML = '';
-  computerGrid.innerHTML = '';
+  commentary.innerText = 'Choose a position to attack';
+  screen.innerText = 'View: Enemy Waters';
   revealDOMGameboard(computerGrid);
   hideDOMGameboard(playerGrid);
 
@@ -52,35 +67,47 @@ function setupNewGame() {
   player2.gameboard.placeShip('cruiser', [7, 5], true);
   player2.gameboard.placeShip('battleship', [0, 3]);
 
+  // Clear DOM elements then render gameboard nodes
+  playerGrid.innerHTML = '';
+  computerGrid.innerHTML = '';
   renderGridPlayer(player1.gameboard.nodes, playerGrid);
   renderGridComputer(player2.gameboard.nodes, computerGrid);
 }
 
 function playRound() {
+  // If it's the computer's turn..
   if (player2.myTurn) {
     tries += 1;
     let randomAttackCoOrds = player2.getRandAttackCoOrds();
     try {
+      // Try a random attack coordinate on the players board
       player1.gameboard.receiveAttack(randomAttackCoOrds);
-      playerGrid.innerText = '';
+
+      // Clear the player's grid and re-render it
+      playerGrid.innerHTML = '';
       renderGridPlayer(player1.gameboard.nodes, playerGrid);
+
+      // Set a delay that hides the players grid, reveals the computers grid and updates the commentary + view
+      if (player1.gameboard.gameOver) {
+        commentary.innerText = `You Lose!`;
+        // setupNewGame();
+        return;
+      }
       setTimeout(() => {
         hideDOMGameboard(playerGrid);
         revealDOMGameboard(computerGrid);
         commentary.innerText = 'Choose a position to attack';
         screen.innerText = 'View: Enemy Waters';
-      }, 1000);
+      }, 600);
+
+      // Switch the player's turn and check if the game is over
       player2.myTurn = false;
       player1.myTurn = true;
-      if (player1.gameboard.gameOver) {
-        alert('You lose!');
-        setupNewGame();
-      }
     } catch (error) {
+      // If the receiveAttack function throws an error because the attack is invalid, re-run the playRound function and set new random coordinates
       randomAttackCoOrds = player2.getRandAttackCoOrds();
       playRound();
     }
-    playRound();
   }
 }
 
@@ -98,20 +125,19 @@ computerGrid.addEventListener('click', (e) => {
       ]);
       computerGrid.innerHTML = '';
       renderGridComputer(player2.gameboard.nodes, computerGrid, player2);
+      if (player2.gameboard.gameOver) {
+        commentary.innerText = `You Win!`;
+        return;
+      }
       setTimeout(() => {
         hideDOMGameboard(computerGrid);
         revealDOMGameboard(playerGrid);
         screen.innerText = 'View: Your Fleet';
       }, 500);
 
-      if (player2.gameboard.gameOver) {
-        alert('You win!');
-        setupNewGame();
-      }
-
       setTimeout(() => {
         playRound();
-      }, 1500);
+      }, 1000);
     }
   } catch (error) {
     alert('Invalid position, try again');
